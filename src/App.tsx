0f0f48fb2c1f8d388a92958a6502c31239f8dc04
digitalworks_hgpark <phk9436@naver.com>
2022-11-07 09:51:39 +0900

    [Feat] typescript 사용을 위한 tsconfig 세팅 및 interface 실습

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor={"teal"} borderColor={"yellow"}/>
+      <Circle bgColor={"#111"} text={"Black"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor || "transparent"}>
+      {text ? `${text}Circle` : 'Circle'}
+    </Container>
+  );
+}
+
+export default Circle;
+
+const Container = styled("div")<CircleProps>`
+  background-color: ${(props) => props.bgColor};
+  border: 3px solid ${(props) => props.borderColor};
+  color: #fff;
+  width: 200px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+`;
