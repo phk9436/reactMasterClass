3bf4602d22d96a077555f9761b56982cfaddbaba
digitalworks_hgpark <phk9436@naver.com>
2023-02-22 15:18:22 +0900

    [Feat] framer-motion 실습 2, variants

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,45 @@
-import { motion } from "framer-motion";
+import { m, motion } from "framer-motion";
 import styled from "styled-components";
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 1,
+      bounce: 0.5,
+      delay: 0.2,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function Home() {
   return (
     <Wrapper>
-      <Box
-      transition={{
-        type: "spring",
-        delay: 0.5
-      }}
-        initial={{
-            scale: 0.5
-        }}
-        animate={{
-            scale: 1,
-            rotate: 360
-        }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
@@ -34,7 +57,18 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const Circle = styled(motion.div)`
+  background-color: #fff;
+  height: 70px;
+  width: 70px;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  place-self: center;
 `;
