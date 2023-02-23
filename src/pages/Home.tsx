73327808be3213b708f09590f1f5be8462535f31
digitalworks_hgpark <phk9436@naver.com>
2023-02-23 15:37:14 +0900

    [Feat] framer-motion 실습 3, gestures

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,39 @@
-import { m, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
+  hover: {
+    rotate: 90,
   },
-  end: {
-    opacity: 1,
+  click: {
     scale: 1,
+    borderRadius: "50%",
+  },
+  drag: {
+    backgroundColor: "rgb(46, 204, 113)",
     transition: {
-      type: "spring",
-      duration: 1,
-      bounce: 0.5,
-      delay: 0.2,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
+      duration: 0.3,
     },
   },
 };
 
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
-};
-
 function Home() {
+  const boxContainerRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <BoxContainer ref={boxContainerRef}>
+        <Box
+          variants={boxVariants}
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={boxContainerRef}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        ></Box>
+      </BoxContainer>
     </Wrapper>
   );
 }
@@ -54,21 +48,21 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BoxContainer = styled.div`
+    width: 400px;
+    height: 400px;
+    background-color: rgba(255,255,255,0.2);
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: #fff;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-`;
-
-const Circle = styled(motion.div)`
-  background-color: #fff;
-  height: 70px;
-  width: 70px;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  place-self: center;
 `;
