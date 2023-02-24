1ac9597615ac7068d30999ede33cf3d12848a412
digitalworks_hgpark <phk9436@naver.com>
2023-02-24 16:59:13 +0900

    [Feat] framer-motion 실습 4, gestures2, useMotionValue & useTransform & useViewportScroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import styled from "styled-components";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 const boxVariants = {
   hover: {
@@ -19,47 +24,61 @@ const boxVariants = {
 };
 
 function Home() {
-  const boxContainerRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotate = useTransform(x, [-200, 0, 200], [-360, 0, 360]);
+  const bg = useTransform(
+    x,
+    [-200, 0, 200],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1.3]);
+  useEffect(() => {
+    //x.onChange(() => console.log(x.get()));
+    //x.onChange(() => console.log(rotate.get()));
+    //scrollYProgress.onChange(() => console.log(scrollYProgress.get()));
+  }, []);
   return (
-    <Wrapper>
-      <BoxContainer ref={boxContainerRef}>
-        <Box
-          variants={boxVariants}
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={boxContainerRef}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        ></Box>
-      </BoxContainer>
+    <Wrapper style={{ backgroundImage: bg }}>
+      <Box
+        variants={boxVariants}
+        drag="x"
+        dragSnapToOrigin
+        dragElastic={0.2}
+        dragConstraints={{ left: -100, right: 100 }}
+        style={{ x, rotate, scale }}
+      ></Box>
+      <button onClick={() => x.set(200)}>click</button>
     </Wrapper>
   );
 }
 
 export default Home;
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 const BoxContainer = styled.div`
-    width: 400px;
-    height: 400px;
-    background-color: rgba(255,255,255,0.2);
-    border-radius: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden;
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
+  position: fixed;
+  top: calc(50% - 100px);
+  left: calc(50% - 100px);
   width: 200px;
   height: 200px;
   background-color: #fff;
