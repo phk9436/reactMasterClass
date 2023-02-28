1995875a29958a70dd468540f263caa5c577a4e2
digitalworks_hgpark <phk9436@naver.com>
2023-02-28 16:56:05 +0900

    [Feat] framer-motion 실습 5, slider & custom & exitBeforeEnter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,45 +3,68 @@ import styled from "styled-components";
 import { useRef, useEffect, useState } from "react";
 
 const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0,
+  initial: (custom: boolean) => {
+    return {
+      opacity: 0,
+      scale: 0,
+      x: custom ? -100 : 100,
+    };
   },
   visible: {
     opacity: 1,
     scale: 1,
-    rotate: 360,
+    x: 0,
     transition: {
-      type: "spring",
-      duration: 1,
-      bounce: 0.5
-    }
+      duration: 0.5,
+    },
   },
-  hiding: {
-    opacity: 0,
-    scale: 0,
-    y: 10,
+  hiding: (custom: boolean) => {
+    return {
+      opacity: 0,
+      scale: 0,
+      x: custom ? 100 : -100,
+      transition: {
+        duration: 0.3
+      }
+    };
   },
 };
 
 function Home() {
-  const [isShow, setIsShow] = useState(false);
-  const changeBox = () => {
-    setIsShow((state) => !state);
+  const SlideArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [visible, setVisible] = useState(0);
+  const [back, setBack] = useState(false);
+  const showNextSlide = () => {
+    setBack(false);
+    setVisible((state) => (state === 9 ? 0 : state + 1));
   };
+
+  const showPrevSlide = () => {
+    setBack(true);
+    setVisible((state) => (state === 0 ? 9 : state - 1));
+  };
+
   return (
     <Wrapper>
-      <button onClick={changeBox}>Click</button>
-      <AnimatePresence>
-        {isShow && (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="hiding"
-          />
+      <AnimatePresence exitBeforeEnter custom={back}>
+        {SlideArr.map(
+          (e, i) =>
+            i === visible && (
+              <Box
+                key={e}
+                variants={boxVariants}
+                initial="initial"
+                animate="visible"
+                exit="hiding"
+                custom={back}
+              >
+                {e}
+              </Box>
+            )
         )}
       </AnimatePresence>
+      <button onClick={showNextSlide}>next</button>
+      <button onClick={showPrevSlide}>prev</button>
     </Wrapper>
   );
 }
@@ -52,21 +75,21 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  padding-top: 30vh;
-  justify-content: flex-start;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const BoxContainer = styled.div`
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
   justify-content: center;
   align-items: center;
-  overflow: hidden;
+  position: relative;
+  button {
+    width: 100px;
+    height: 40px;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    position: absolute;
+    bottom: 20%;
+    &:nth-of-type(2) {
+      bottom: calc(20% + 50px);
+    }
+  }
 `;
 
 const Box = styled(motion.div)`
@@ -75,4 +98,9 @@ const Box = styled(motion.div)`
   background-color: #fff;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
 `;
