d5713b659959a5f9f38010ea4d6d7df644f343f1
digitalworks_hgpark <phk9436@naver.com>
2023-03-02 16:45:46 +0900

    [Feat] framer-motion 실습 7, layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,51 +2,50 @@ import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 import { useRef, useEffect, useState } from "react";
 
-const boxVariants = {
-  initial: (custom: boolean) => {
-    return {
-      opacity: 0,
-      scale: 0,
-      x: custom ? -100 : 100,
-    };
+const overlay = {
+  hidden: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
   visible: {
-    opacity: 1,
-    scale: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-    },
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
-  hiding: (custom: boolean) => {
-    return {
-      opacity: 0,
-      scale: 0,
-      x: custom ? 100 : -100,
-      transition: {
-        duration: 0.3,
-      },
-    };
+  exit: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
   },
 };
 
 function Home() {
-  const [isClicked, setIsClicked] = useState(false);
-  const toggleClicked = () => setIsClicked((prev) => !prev);
+  const boxArr = [1, 2, 3, 4];
+  
+  const [clicked, setClicked] = useState(false);
+  const [id, setId] = useState<null | string>(null);
+
+  const toggle = (data?: string) => {
+    data ? setId(data) : setId(null);
+    setClicked((state) => !state);
+  };
+  
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box >
-        <div></div>
-        {!isClicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
-        ) : null}
-      </Box>
-      <Box>
-        <div></div>
-        {isClicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
-        ) : null}
-      </Box>
+    <Wrapper>
+      <Grid>
+        {boxArr.map((e, i) => (
+          <Box key={i} onClick={() => toggle(`${e}`)} layoutId={`${e}`} />
+        ))}
+      </Grid>
+
+      <AnimatePresence>
+        {id && (
+          <Overlay
+            variants={overlay}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            onClick={() => toggle()}
+          >
+            <Box layoutId={id} style={{ width: 240 }}/>
+          </Overlay>
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
@@ -59,36 +58,32 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
-  button {
-    width: 100px;
-    height: 40px;
-    border-radius: 10px;
-    border: none;
-    outline: none;
-    position: absolute;
-    bottom: 20%;
-    &:nth-of-type(2) {
-      bottom: calc(20% + 50px);
-    }
-  }
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  height: 50px;
   background-color: #fff;
-  border-radius: 40px;
+  border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 300px;
+  gap: 5px;
+
+  div:where(:first-child, :last-child) {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
-`;
-
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
